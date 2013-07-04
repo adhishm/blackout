8288568c9054d8c38bcb243d07c9644a58e8a257
Adhish Majumdar <adhishm@gmail.com>
2013-07-04 19:46:26 +0200

    Added license text

--- gameManager.h
+++ gameManager.h
@@ -7,6 +7,27 @@
  *@details This file contains function declarations for the game manager. The game manager manages the gameplay by calling appropriate functions depending on the interaction with the player.
  */
 
+/*
+    DD2D-Matryoshka approach.
+    A set of classes defining the behaviour of crystalline defects,
+    with the final goal of carrying out dislocation dynamics simulations
+    in two dimensions.
+    Copyright (C) 2013  Adhish Majumdar
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef GAMEMANAMAGER_H
 #define GAMEMANAGER_H
 
